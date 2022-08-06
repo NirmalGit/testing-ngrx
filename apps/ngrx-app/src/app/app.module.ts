4f02ec7c467d4394227b8f5e0b153733390a0a5b
NirmalGit <nirsha2007@gmail.com>
2022-08-06 10:53:19 +0530

    store and devtools install

--- apps/ngrx-app/src/app/app.module.ts
+++ apps/ngrx-app/src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}), StoreDevtoolsModule.instrument({
+    maxAge: 25,
+    logOnly: false,
+    autoPause: true,
+    features: {
+      pause: false,
+      lock: true,
+      persist: true
+    }
+  })],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
