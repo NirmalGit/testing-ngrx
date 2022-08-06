de97cf727653f948e3ba5689829dbc48debef61e
NirmalGit <nirsha2007@gmail.com>
2022-08-06 10:40:02 +0530

    post component created

--- libs/posts/src/lib/posts.module.ts
+++ libs/posts/src/lib/posts.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   imports: [
@@ -10,5 +11,6 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [PostsComponent],
 })
 export class PostsModule {}
