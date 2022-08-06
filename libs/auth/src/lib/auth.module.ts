ec1c8eeb8b937575d0416b7989611a506808b40c
NirmalGit <nirsha2007@gmail.com>
2022-08-06 14:03:08 +0530

    auth login form added

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './containers/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   imports: [
@@ -10,5 +12,6 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [LoginComponent, LoginFormComponent],
 })
 export class AuthModule {}
